934773399969670ba7aa3a72970c2040ef6b98e6
Juuso <juuso.melentjeff@gmail.com>
2024-11-28 22:32:25 +0200

    make everything even betterer

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-import smtplib
-from email.mime.text import MIMEText
 import os
-from dotenv import load_dotenv
-from bs4 import BeautifulSoup
 import requests
-from rich.console import Console
 import datetime
 import time
+import logging
 
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+from rich.console import Console
 from rich.table import Table
+from email.mime.text import MIMEText
+import smtplib
 
-console = Console()
-rows = []
+logger = logging.getLogger(__name__)
+
+
 load_dotenv()
 
 
@@ -43,62 +45,38 @@ def send_email_with_smtp(msg):
         raise Exception(f"An unexpected error occurred: {e}")
 
 
-def check_prices():
-    urls = [
-        "https://www.crema.fi/fi/products/1zpresso/j-ultra-foldable-grinder/12264",
-        "https://www.crema.fi/fi/products/lucaffe/mamma-lucia/364",
-        "https://www.crema.fi/fi/products/1zpresso/k-ultra-foldable-grinder/11887",
-        "https://www.crema.fi/fi/products/planetary-design/airscape-classic-charcoal/4872",
-        "https://www.crema.fi/fi/products/fellow/atmos-vacuum-canister-steel/3997",
-        "https://www.crema.fi/fi/products/fellow/stagg-ekg-pour-over-kettle/3924",
-        "https://www.crema.fi/fi/products/timemore/glass-server/3640",
-        "https://www.crema.fi/fi/products/fellow/stagg-double-wall-carafe/2575",
-        "https://www.crema.fi/fi/products/hario/v60-barista-server/13117",
-    ]
-    while True:
-        table = build_table()
-        try:
-            for url in urls:
-                response = requests.get(url)
-                if response.status_code == 200:
-                    page_content = response.content
-
-                    soup = BeautifulSoup(page_content, "html.parser")
-
-                    product_name = soup.find("h1")
-
-                    normal_price = soup.select("[id^='normal-price']")
-                    discount_price = soup.select("[id^='discounted-price']")
-
-                    normal_price_text = None
-                    for price in normal_price:
-                        normal_price_text = price.text
-                        break
-                    discount_price_text = None
-                    if discount_price:
-                        for discount in discount_price:
-                            discount_price_text = discount.text
-                            break
-
-                    now = datetime.datetime.now()
-                    time_string = now.strftime("%H:%M:%S")
-                    table.add_row(
-                        time_string,
-                        product_name.text,
-                        normal_price_text,
-                        discount_price_text,
-                    )
-                    console.clear()
-                    console.print(table)
-                    time.sleep(1)
-                else:
-                    print("Failed to fetch the page")
-        except Exception as e:
-            raise Exception(e)
-        time.sleep(900)
+def fetch_page_content(url: str):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    else:
+        logger.error(f"Failed to fetch {url}: {response.status_code}")
+        return None
 
 
-def build_table():
+def parse_page_content(content: bytes) -> dict:
+    soup = BeautifulSoup(content, "html.parser")
+    product_name = soup.find("h1").text
+    normal_price_text = None
+    discount_price_text = None
+
+    for price in soup.select("[id^='normal-price']"):
+        normal_price_text = price.text
+        break
+
+    if soup.select("[id^='discounted-price']"):
+        for discount in soup.select("[id^='discounted-price']"):
+            discount_price_text = discount.text
+            break
+
+    return {
+        "product_name": product_name,
+        "normal_price": normal_price_text,
+        "discount_price": discount_price_text,
+    }
+
+
+def build_table() -> Table:
     table = Table(show_header=True, header_style="bold magenta")
     table.add_column("Timestamp", style="dim", width=12)
     table.add_column("Product")
@@ -107,9 +85,44 @@ def build_table():
     return table
 
 
-def main():
-    check_prices()
+def main(urls: list) -> None:
+    console = Console()
+    try:
+        while True:
+            table = build_table()
+            for url in urls:
+                content = fetch_page_content(url)
+                if content is not None:
+                    data = parse_page_content(content)
+                    now = datetime.datetime.now()
+                    time_string = now.strftime("%H:%M:%S")
+                    table.add_row(
+                        time_string,
+                        data["product_name"],
+                        data["normal_price"],
+                        data["discount_price"],
+                    )
+                    console.clear()
+                    console.print(table)
+                    time.sleep(1)
+
+            # Pause for 15 minutes before updating again
+            time.sleep(900)
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        raise
 
 
 if __name__ == "__main__":
-    main()
+    main(
+        [
+            "https://www.crema.fi/fi/products/1zpresso/j-ultra-foldable-grinder/12264",
+            "https://www.crema.fi/fi/products/1zpresso/k-ultra-foldable-grinder/11887",
+            "https://www.crema.fi/fi/products/planetary-design/airscape-classic-charcoal/4872",
+            "https://www.crema.fi/fi/products/fellow/atmos-vacuum-canister-steel/3997",
+            "https://www.crema.fi/fi/products/fellow/stagg-ekg-pour-over-kettle/3924",
+            "https://www.crema.fi/fi/products/timemore/glass-server/3640",
+            "https://www.crema.fi/fi/products/fellow/stagg-double-wall-carafe/2575",
+            "https://www.crema.fi/fi/products/hario/v60-barista-server/13117",
+        ]
+    )
